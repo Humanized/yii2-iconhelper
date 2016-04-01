513e7ad02c74c72e89404d255b314e5ed7b7f779
jeffrey <jeffrey@localhost.localdomain>
2016-04-01 22:22:15 +0200

    [ADD] Preperation for Nano - Old Style

--- IconHelper.php
+++ IconHelper.php
@@ -15,13 +15,14 @@ class IconHelper
      */
     public static function getIcon($input, array $config = [])
     {
+
         //Lookup DB Value corresponding to input
-        $model = models\icons\IconRegister::findOne(['name' => $input]);
+        $model = models\IconRegister::findOne(['name' => $input]);
 
         if (!isset($model) || $model->framework_id == 'bdg') {
-            return Html::badge($input, $config);
+            return Html::badge(isset($model) ? $model->icon : $input, $config);
         }
-        return Html::icon($icon, $config, ($model->framework_id == 'bsg' ? 'glyphicon glyphicon-' : 'fa fa-'));
+        return Html::badge(Html::icon($model->icon, $config, ($model->framework_id == 'bsg' ? 'glyphicon glyphicon-' : 'fa fa-'))) . (isset($config['label']) ? ' ' . $config['label'] : '');
     }
 
 }
--- models/IconFramework.php
+++ models/IconFramework.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace humanized\iconhelper\models\icons;
+namespace humanized\iconhelper\models;
 
 use Yii;
 
